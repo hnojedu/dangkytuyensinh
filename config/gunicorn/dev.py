3ddb971f41f8234ef7195f0d282da1dfb678e3cd
LetterC67 <hoangdeptoong@gmail.com>
2023-05-15 11:13:09 +0700

    zzz

--- config/gunicorn/dev.py
+++ config/gunicorn/dev.py
@@ -7,6 +7,6 @@ workers = multiprocessing.cpu_count() + 1
 threads = 2
 bind = "0.0.0.0:8000"
 reload = True
-accesslog = errorlog = "/var/log/gunicorn/dev.log"
+accesslog = errorlog = "log/gunicorn/dev.log"
 capture_output = True
 daemon = True
